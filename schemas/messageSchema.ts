05d6c7cb1638ea41d75b967d7076ad3a63e05e70
anandromy <23f3003270@ds.study.iitm.ac.in>
2024-06-05 06:50:38 +0530

    zod schemas

---
+++ schemas/messageSchema.ts
@@ -0,0 +1,9 @@
+import z from "zod"
+
+export const messageSchema = z.object({
+    content: z
+    .string()
+    .min(10, { message: "Message content must be atleast of 10 characters" })
+    .max(300, { message: "Message content must be no longer than 300 characters" })
+    
+})
